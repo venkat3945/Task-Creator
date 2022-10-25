f2a69850a7832ba9fafd8d04951df82524a317c6
venkat3945 <86202099+venkat3945@users.noreply.github.com>
2022-10-25 10:10:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Taskcreator from "./practice/Taskcreator";
+const App = () =>{
+    return(
+        <div>
+                <Taskcreator />
+        </div>
+    )
+}
+export default App;
